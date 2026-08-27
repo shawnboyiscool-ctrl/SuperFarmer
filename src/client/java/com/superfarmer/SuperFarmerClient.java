8ad7c30f46c48ee0a6dffdd5ad12416170a5bba1
shawnboyiscool-ctrl <shawnboyiscool@gmail.com>
2026-08-27 15:18:05 -0500

    Use custom Super Farmer renderer

--- src/client/java/com/superfarmer/SuperFarmerClient.java
+++ src/client/java/com/superfarmer/SuperFarmerClient.java
@@ -2,11 +2,10 @@ package com.superfarmer;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.renderer.entity.VillagerRenderer;
 
 public class SuperFarmerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRenderers.register(ModEntityTypes.SUPER_FARMER, VillagerRenderer::new);
+        EntityRenderers.register(ModEntityTypes.SUPER_FARMER, SuperFarmerRenderer::new);
     }
 }
